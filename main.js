11afdda9f221f3dfdab639d6bc0bf1f42db1d448
adhritdc <82810967+adhritdc@users.noreply.github.com>
2021-04-24 11:20:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+function bbba() {
+    document.getElementById("li1").style.backgroundColor = "red";
+}
 
+function bbbb() {
+    document.getElementById("li2").style.backgroundColor = "red";
+}
+
+function bbbc() {
+    document.getElementById("li3").style.backgroundColor = "red";
+}
+
+function bbbd() {
+    document.getElementById("li4").style.backgroundColor = "red";
+}
+
+function bbbe() {
+    document.getElementById("li5").style.backgroundColor = "red";
+}
+
+function gg1() {
+    document.getElementById("li5").style.backgroundColor = "transparent";
+}
+
+function gg2() {
+    document.getElementById("li4").style.backgroundColor = "transparent";
+}
+
+function gg3() {
+    document.getElementById("li3").style.backgroundColor = "transparent";
+}
+
+function gg4() {
+    document.getElementById("li2").style.backgroundColor = "transparent";
+}
+
+function gg5() {
+    document.getElementById("li1").style.backgroundColor = "transparent";
+}
+
+function ttt1() {
+    document.getElementById("li1").style.backgroundColor = "white";
+}
